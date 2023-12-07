2895dd04e2ba55aa99ee89590f6a5604749ac3aa
Ishii <ismaelv@protonmail.com>
2023-12-06 22:32:20 -0800

    Implemented a button that will functin as a switcher for the direction of the placement of the pieces for the user.

---
+++ src/Render.js
@@ -0,0 +1,142 @@
+function generateBoard() {
+    const board = document.createElement("div");
+    board.classList.add("board");
+    for (let i = 0; i < 10; i++) {
+        const column = document.createElement("div");
+        column.classList.add("board-row");
+        board.appendChild(column);
+        for (let j = 0; j < 10; j++) {
+            const row = document.createElement("div");
+            row.classList.add("board-tile");
+            row.dataset.column = i;
+            row.dataset.row = j;
+            column.appendChild(row);
+        }
+    }
+    return board;
+}
+
+function addShip(board, ship) {
+    ship.forEach(function fillTile(coord) {
+        const tile = board.querySelector(
+            `[data-column="${coord.y}"][data-row="${coord.x}"]`
+        );
+        tile.classList.remove("board-tile");
+        tile.classList.add("board-tile-ship");
+    });
+}
+
+function playerBoardShot(board, coord) {
+    const tile = board.querySelector(
+        `[data-column="${coord.y}"][data-row="${coord.x}"]`
+    );
+
+    if (tile.classList.contains("board-tile-ship")) {
+        tile.classList.remove("board-tile-ship");
+        tile.classList.add("board-tile-ship-shot");
+    } else {
+        tile.classList.remove("board-tile");
+        tile.classList.add("board-tile-shot");
+    }
+}
+
+function shipHit(board, coord) {
+    const tile = board.querySelector(
+        `[data-column="${coord.y}"][data-row="${coord.x}"]`
+    );
+
+    tile.classList.remove("board-tile-ship");
+    tile.classList.add("board-tile-ship-shot");
+}
+
+function flushShip(board, ar) {
+    ar.forEach((coord) => {
+        playerBoardShot(board, coord);
+    });
+}
+
+async function coordinateAttack(board) {
+    let coord;
+    const tiles = board.querySelectorAll(".board-tile");
+
+    await new Promise((myResolve) => {
+        tiles.forEach((node) => {
+            node.addEventListener("click", () => {
+                node.classList.remove("board-tile");
+                node.classList.add("board-tile-shot");
+                if (node.classList.contains("board-tile-ship")) {
+                    node.classList.remove("board-tile-ship");
+                    node.classList.remove("board-tile-shot");
+                    node.classList.add("board-tile-ship-shot");
+                }
+                tiles.forEach((node) => {
+                    node.replaceWith(node.cloneNode(false));
+                });
+                myResolve({ y: node.dataset.column, x: node.dataset.row });
+            });
+        });
+    }).then((value) => {
+        coord = value;
+    });
+    return coord;
+}
+
+function generateBottomPieces() {
+    const container = document.createElement("div");
+    container.classList.add("footer-container");
+    const piecesContainer = document.createElement("div");
+    piecesContainer.classList.add("pieces-container");
+    const topRow = document.createElement("div");
+    topRow.classList.add("pieces-top-row");
+    const botRow = document.createElement("div");
+    botRow.classList.add("pieces-bot-row");
+
+    const blockGenerator = (size) => {
+        const block = document.createElement("div");
+        block.classList.add("pieces-block");
+        block.setAttribute("draggable", "true");
+        for (let i = 0; i < size; i++) {
+            const tile = document.createElement("div");
+            tile.classList.add("board-tile-footer");
+            block.appendChild(tile);
+        }
+        return block;
+    };
+
+    topRow.append(blockGenerator(2), blockGenerator(3), blockGenerator(3));
+    botRow.append(blockGenerator(4), blockGenerator(5));
+    piecesContainer.append(topRow, botRow);
+    container.append(piecesContainer);
+    return container;
+}
+
+function generateDirectionButton() {
+    const direction = document.createElement("div");
+    const directionButton = document.createElement("div");
+    directionButton.innerText = "X";
+    directionButton.dataset.direction = "0";
+    directionButton.classList.add("direction-button");
+    direction.classList.add("direction-button-container");
+    directionButton.addEventListener("click", function () {
+        if (this.innerText === "X") {
+            this.innerText = "Y";
+            this.dataset.direction = "1";
+        } else if (this.innerText === "Y") {
+            this.innerText = "X";
+            this.dataset.direction = "0";
+        }
+    });
+    direction.append(directionButton);
+    return direction;
+}
+
+module.exports = {
+    generateBoard,
+    addShip,
+    shipHit,
+    flushShip,
+    coordinateAttack,
+    playerBoardShot,
+    generateBottomPieces,
+    generateDirectionButton,
+};
--- src/index.js
+++ src/index.js
@@ -2,111 +2,29 @@
 require("./style.css");
 const Player = require("./Player");
 const PlayerAI = require("./PlayerAI");
-
-function generateBoard() {
-    const board = document.createElement("div");
-    board.classList.add("board");
-    for (let i = 0; i < 10; i++) {
-        const column = document.createElement("div");
-        column.classList.add("board-row");
-        board.appendChild(column);
-        for (let j = 0; j < 10; j++) {
-            const row = document.createElement("div");
-            row.classList.add("board-tile");
-            row.dataset.column = i;
-            row.dataset.row = j;
-            column.appendChild(row);
-        }
-    }
-    return board;
-}
-
-function addShip(board, ship) {
-    ship.forEach(function fillTile(coord) {
-        const tile = board.querySelector(
-            `[data-column="${coord.y}"][data-row="${coord.x}"]`
-        );
-        tile.classList.remove("board-tile");
-        tile.classList.add("board-tile-ship");
-    });
-}
-
-function playerBoardShot(board, coord) {
-    const tile = board.querySelector(
-        `[data-column="${coord.y}"][data-row="${coord.x}"]`
-    );
-
-    if (tile.classList.contains("board-tile-ship")) {
-        tile.classList.remove("board-tile-ship");
-        tile.classList.add("board-tile-ship-shot");
-    } else {
-        tile.classList.remove("board-tile");
-        tile.classList.add("board-tile-shot");
-    }
-}
-
-function shipHit(board, coord) {
-    const tile = board.querySelector(
-        `[data-column="${coord.y}"][data-row="${coord.x}"]`
-    );
-
-    tile.classList.remove("board-tile-ship");
-    tile.classList.add("board-tile-ship-shot");
-}
-
-function flushShip(board, ar) {
-    ar.forEach((coord) => {
-        playerBoardShot(board, coord);
-    });
-}
-
-async function coordinateAttack(board) {
-    let coord;
-    const tiles = board.querySelectorAll(".board-tile");
-
-    await new Promise((myResolve) => {
-        tiles.forEach((node) => {
-            node.addEventListener("click", () => {
-                node.classList.remove("board-tile");
-                node.classList.add("board-tile-shot");
-                if (node.classList.contains("board-tile-ship")) {
-                    node.classList.remove("board-tile-ship");
-                    node.classList.remove("board-tile-shot");
-                    node.classList.add("board-tile-ship-shot");
-                }
-                tiles.forEach((node) => {
-                    node.replaceWith(node.cloneNode(false));
-                });
-                myResolve({ y: node.dataset.column, x: node.dataset.row });
-            });
-        });
-    }).then((value) => {
-        coord = value;
-    });
-    return coord;
-}
+const Render = require("./Render");
 
 async function game() {
     const player = new Player();
     player.board.presetPlaceShips(0);
     const playerAI = new PlayerAI();
-    player.boardDOM = generateBoard();
-    playerAI.boardDOM = generateBoard();
+    player.boardDOM = Render.generateBoard();
+    playerAI.boardDOM = Render.generateBoard();
     player.board.ships.forEach((ship) => {
-        addShip(player.boardDOM, ship.getCoords());
+        Render.addShip(player.boardDOM, ship.getCoords());
     });
     const boardSpace = document.getElementById("board-space");
     boardSpace.appendChild(player.boardDOM);
     boardSpace.appendChild(playerAI.boardDOM);
     while (!player.board.hasLost() && !playerAI.board.hasLost()) {
-        const playerShoot = await coordinateAttack(playerAI.boardDOM);
+        const playerShoot = await Render.coordinateAttack(playerAI.boardDOM);
         const boardReact = playerAI.board.recieveAttack(playerShoot);
         if (boardReact instanceof Array) {
             boardReact.forEach((coord) => {
-                shipHit(playerAI.boardDOM, coord);
+                Render.shipHit(playerAI.boardDOM, coord);
             });
         } else if (boardReact instanceof Object)
-            shipHit(playerAI.boardDOM, boardReact);
+            Render.shipHit(playerAI.boardDOM, boardReact);
 
         let enemyLandedHit = null;
         let aiCoordinatedAttack = null;
@@ -116,12 +34,12 @@ async function game() {
         }
         if (enemyLandedHit instanceof Array) {
             playerAI.flushMarks(enemyLandedHit);
-            flushShip(player.boardDOM, enemyLandedHit);
+            Render.flushShip(player.boardDOM, enemyLandedHit);
         } else if (enemyLandedHit instanceof Object) {
             playerAI.markAttack(enemyLandedHit);
-            playerBoardShot(player.boardDOM, enemyLandedHit);
+            Render.playerBoardShot(player.boardDOM, enemyLandedHit);
         } else {
-            playerBoardShot(player.boardDOM, aiCoordinatedAttack);
+            Render.playerBoardShot(player.boardDOM, aiCoordinatedAttack);
         }
     }
     if (playerAI.board.hasLost()) {
@@ -136,4 +54,9 @@ async function game() {
 }
 
 const body = document.getElementsByTagName("body")[0];
+const footer = document.getElementById("footer");
+playerFooter = Render.generateBottomPieces();
+playerFooter.appendChild(Render.generateDirectionButton());
+footer.appendChild(playerFooter);
+footer.appendChild(Render.generateBottomPieces());
 game();
