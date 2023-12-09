119b86dcf9dd762b3a7ad6b3b555eb808d1f85a0
Ishii <ismaelv@protonmail.com>
2023-12-09 04:12:40 -0800

    Player names now display, and title too

--- src/index.js
+++ src/index.js
@@ -9,20 +9,21 @@ async function game() {
     // game setup
     const player = new Player();
     const playerAI = new PlayerAI();
+    document.getElementById(
+        "name-container"
+    ).innerHTML = `<div>${player.name}</div><div>${playerAI.name}</div>`;
     document
         .getElementById("board-space")
         .append(player.boardDOM, playerAI.boardDOM);
 
-    // Generate the footer that the user will be able use
+    // Generate the footer that the user will use to set pieces
     const footer = document.getElementById("footer");
     footer.append(
         Render.generateBottomPieces(true), // true enables player drag
         Render.generateBottomPieces()
     );
-
     // create drag event listeners
     const allUserBlocks = document.querySelectorAll('[draggable="true"');
-
     const dragBank = [];
     for (block of allUserBlocks) {
         dragBank.push(Render.loadDrag(block, player));
